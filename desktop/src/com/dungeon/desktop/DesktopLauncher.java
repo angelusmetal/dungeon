129d1888002b995b3ebe5caff56d9ed44e3a2ea7
Rodrigo Fernandez <angelusmetal@gmail.com>
2018-04-21 19:30:42 -0300

    Now reads a toml configuration file, using on toml4j.

--- desktop/src/com/dungeon/desktop/DesktopLauncher.java
+++ desktop/src/com/dungeon/desktop/DesktopLauncher.java
@@ -2,13 +2,17 @@ package com.dungeon.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dungeon.engine.resource.TextureResource;
 import com.dungeon.game.Dungeon;
+import com.moandjiezana.toml.Toml;
+
+import java.io.InputStream;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1600;
-		config.height = 800;
+		// Read configuration from file
+		Toml toml = new Toml().read(TextureResource.class.getClassLoader().getResourceAsStream("config.toml"));
+		LwjglApplicationConfiguration config = toml.getTable("application").to(LwjglApplicationConfiguration.class);
 		new LwjglApplication(new Dungeon(), config);
 	}
 }
