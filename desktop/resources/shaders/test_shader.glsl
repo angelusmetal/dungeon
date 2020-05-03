f1fb25df037befc596ecc4f23c631ce8a8df582d
Rodrigo Fernandez <angelusmetal@gmail.com>
2020-05-03 17:16:19 -0300

    Added a main class for testing lighting shader

--- desktop/resources/shaders/test_shader.glsl
+++ desktop/resources/shaders/test_shader.glsl
@@ -5,21 +5,119 @@ precision mediump int;
 
 uniform sampler2D u_texture;
 
-// Coords, size & color of sphere
-uniform vec2 u_coord;
-uniform float u_size;
-uniform vec4 u_color;
-uniform float u_ratio;
+// Buffer size
+uniform vec2 u_bufferSize;
+
+// Light
+uniform float u_lightRange;
+uniform float u_lightRadius;
+uniform vec2 u_lightOrigin;
+uniform vec4 u_lightColor;
+uniform float lightHardness = 1.0;
+
+// Geometry
+uniform vec4 u_segments[256];
+uniform int u_segmentCount;
+
+// Casting
+uniform int u_sampleCount;
 
 varying vec4 v_color;
 varying vec2 v_texCoord;
 
-void main() {
-	vec4 base = texture2D(u_texture, v_texCoord.xy) * v_color;
-	vec2 dist = u_coord - v_texCoord.xy;
-	dist.x *= u_ratio;
-	// Correct ratio
-	float i = 1.0 - length(dist) / u_size;
+// Given three colinear points p, q, r, the function checks if
+// point q lies on line segment 'pr'
+bool onSegment(vec2 p, vec2 q, vec2 r)
+{
+	return q.x == clamp(q.x, p.x, r.x) && q.y == clamp(q.y, p.y, r.y);
+//	return step(q.x, p.x) * step(q.x, r.x) * step(q.y, p.y) * step(q.y, r.y);
+//    return (q.x <= max(p.x, r.x) && q.x >= min(p.x, r.x) &&
+//        q.y <= max(p.y, r.y) && q.y >= min(p.y, r.y));
+}
 
-    gl_FragColor = base * (1-i) + u_color * i;
+// To find orientation of ordered triplet (p, q, r).
+// The function returns following values
+// 0 --> p, q and r are colinear
+// 1 --> Clockwise
+// 2 --> Counterclockwise
+float orientation(vec2 p, vec2 q, vec2 r)
+{
+    // See https://www.geeksforgeeks.org/orientation-3-ordered-points/
+    // for details of below formula.
+    return  (q.y - p.y) * (r.x - q.x) -
+            (q.x - p.x) * (r.y - q.y);
+
+//    if (val == 0) return 0; // colinear
+//
+//    return (val > 0)? 1: 2; // clock or counterclock wise
+}
+
+// The main function that returns true if line segment 'p1q1'
+// and 'p2q2' intersect.
+bool doIntersect(vec2 p1, vec2 q1, vec2 p2, vec2 q2)
+{
+    // Find the four orientations needed for general and
+    // special cases
+    float o1 = orientation(p1, q1, p2);
+    float o2 = orientation(p1, q1, q2);
+    float o3 = orientation(p2, q2, p1);
+    float o4 = orientation(p2, q2, q1);
+
+//    return (o1 != o2 && o3 != o4)
+    // General case
+    return (o1*o2 < 0.0 && o3*o4 < 0.0);
+//    // Special Cases
+//    // p1, q1 and p2 are colinear and p2 lies on segment p1q1
+//    || (o1 == 0 && onSegment(p1, p2, q1))
+//
+//    // p1, q1 and q2 are colinear and q2 lies on segment p1q1
+//    || (o2 == 0 && onSegment(p1, q2, q1))
+//
+//    // p2, q2 and p1 are colinear and p1 lies on segment p2q2
+//    || (o3 == 0 && onSegment(p2, p1, q2))
+//
+//    // p2, q2 and q1 are colinear and q1 lies on segment p2q2
+//    || (o4 == 0 && onSegment(p2, q1, q2));
+}
+
+float sampleLight(vec2 coord, vec2 lightSampleOrigin) {
+	// Iterate segments for intersection
+	for (int i = 0; i < u_segmentCount; ++i) {
+		if (doIntersect(coord, lightSampleOrigin, u_segments[i].xy, u_segments[i].zw)) {
+			return 0.0; // Intersection; return shadow
+		}
+	}
+	// No intersection; return light with appropriate attenuation
+	float dist = length(coord - lightSampleOrigin);
+	return clamp((1.0 - dist / u_lightRange) * lightHardness, 0.0, 1.0);
+}
+
+//8200
+void main() {
+	vec2 coord = v_texCoord.xy * u_bufferSize;
+	float lightPerSample = 1.0 / u_sampleCount;
+
+	vec2 normal = normalize(coord - u_lightOrigin);
+	normal.yx = normal.xy;
+	normal.x *= -1;
+	float luminosity = 0;
+
+	vec2 spread = u_lightRadius * 2 / u_sampleCount * normal;
+	float offset = 0 - (u_sampleCount - 1) * 0.5;
+	for (float s; s < u_sampleCount; ++s) {
+		luminosity += sampleLight(coord, u_lightOrigin + spread * (offset + s)) * lightPerSample;
+	}
+	gl_FragColor = v_color * (1.0-luminosity) + u_lightColor * luminosity;
+
+	// Display light radius as dark circle
+	if (length(u_lightOrigin - coord) < u_lightRadius) {
+		gl_FragColor *= 0.8;
+	}
+
+	// Awful hack to prevent the shader from breaking
+    if (v_texCoord.x == 1.0 && v_texCoord.y == 1.0) {
+    	gl_FragColor = texture2D(u_texture, v_texCoord.xy) * v_color;
+    	gl_FragColor.a = luminosity;
+    }
+
 }
--- desktop/src/com/dungeon/desktop/ShaderDebugger.java
+++ desktop/src/com/dungeon/desktop/ShaderDebugger.java
@@ -2,6 +2,8 @@ package com.dungeon.desktop;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.graphics.Color;
@@ -13,25 +15,35 @@ import com.badlogic.gdx.math.Vector2;
 import com.dungeon.engine.util.Rand;
 import com.dungeon.game.resource.Resources;
 
-public class ShaderDebugger extends ApplicationAdapter {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ShaderDebugger extends ApplicationAdapter implements InputProcessor {
 
 	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-//		config.vSyncEnabled = false;
-//		config.foregroundFPS = 0;
+		config.vSyncEnabled = false;
+		config.foregroundFPS = 0;
 		config.width = 1800;
 		config.height = 900;
 		new LwjglApplication(new ShaderDebugger(), config);
 	}
+
 
 	ShaderProgram shaderProgram;
 	SpriteBatch batch;
 	Color color = Color.RED;
 	Texture pixel;
 	Vector2 speed = new Vector2();
-	Vector2 coord = new Vector2(Rand.nextFloat(1f), Rand.nextFloat(1f));
-	float ratio;
+	Vector2 lightOrigin = new Vector2();
+	float lightRadius = 10f;
+	private float lightRange = 1200f;
+	private int sampleCount = 30;
+	Vector2 bufferSize = new Vector2();
 	float time, lastLog;
+	float[] geometry = new float[1024];
+	int nextGeometry = 16;
+	Vector2 startVector = new Vector2();
 
 	@Override
 	public void create () {
@@ -39,35 +51,46 @@ public class ShaderDebugger extends ApplicationAdapter {
 		shaderProgram = Resources.shaders.get("df_vertex.glsl|test_shader.glsl");
 		pixel = new Texture("core/assets/fill.png");
 		batch = new SpriteBatch();
-		speed.x = 2f / Gdx.graphics.getWidth();
-		speed.y = 2f / Gdx.graphics.getHeight();
-		ratio = (float) Gdx.graphics.getWidth() / Gdx.graphics.getHeight();
+		speed.set(0.03f, 0.03f);
+		resize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		Gdx.input.setInputProcessor(this);
+		System.arraycopy(new float[] {
+				800, 350, 1000, 350,
+				1000, 350, 1000, 550,
+				1000, 550, 800, 550,
+				800, 550, 800, 350
+		}, 0, geometry, 0, 16);
+
 	}
 
 	@Override
 	public void render() {
 		shaderProgram.begin();
-		shaderProgram.setUniformf("u_color", color);
-		shaderProgram.setUniformf("u_size", 0.08f);
-		shaderProgram.setUniformf("u_coord", coord);
-		shaderProgram.setUniformf("u_ratio", ratio);
+		shaderProgram.setUniformf("u_bufferSize", bufferSize);
+		shaderProgram.setUniformf("u_lightColor", color);
+		shaderProgram.setUniformf("u_lightRadius", lightRadius);
+		shaderProgram.setUniformf("u_lightRange", lightRange);
+		shaderProgram.setUniformf("u_lightOrigin", lightOrigin);
+		shaderProgram.setUniform4fv("u_segments[0]", geometry, 0, geometry.length);
+		shaderProgram.setUniformi("u_segmentCount", geometry.length / 4);
+		shaderProgram.setUniformi("u_sampleCount", sampleCount);
 		shaderProgram.end();
 		batch.setShader(shaderProgram);
 		batch.begin();
 		batch.setColor(Color.argb8888(0.1f, 0.3f, 0.5f, 1f));
-		batch.draw(pixel, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch.draw(pixel, 0, 0, bufferSize.x, bufferSize.y);
 		Gdx.gl.glClearColor(0f, 0f, 0f, 1f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.end();
-		coord.add(speed);
-		if (coord.x < 0 || coord.x > 1) {
+//		lightOrigin.add(speed);
+		if (lightOrigin.x < 0 || lightOrigin.x > bufferSize.x) {
 			speed.x *= -1;
 		}
-		if (coord.y < 0 || coord.y > 1) {
+		if (lightOrigin.y < 0 || lightOrigin.y > bufferSize.y) {
 			speed.y *= -1;
 		}
 		time += Gdx.graphics.getDeltaTime();
-		if (time > lastLog) {
+		if (time > lastLog + 1.0) {
 			lastLog = time;
 			System.out.println(Gdx.graphics.getFramesPerSecond());
 		}
@@ -75,13 +98,74 @@ public class ShaderDebugger extends ApplicationAdapter {
 
 	@Override
 	public void resize(int width, int height) {
-		speed.x = 2f / Gdx.graphics.getWidth();
-		speed.y = 2f / Gdx.graphics.getHeight();
-		ratio = (float) Gdx.graphics.getWidth() / Gdx.graphics.getHeight();
+		lightOrigin.set(Rand.nextFloat(width), Rand.nextFloat(height));
+		bufferSize.set(width, height);
+		System.out.println("Resolution: " + bufferSize);
 	}
 	@Override
 	public void dispose() {
 		batch.dispose();
 		Resources.dispose();
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		if (keycode == Input.Keys.PLUS) {
+			sampleCount++;
+		} else if (keycode == Input.Keys.MINUS) {
+			sampleCount--;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		if (button == 0) {
+			startVector.set(screenX, screenY);
+		}
+		System.out.println(button);
+		return true;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		if (button == 0) {
+			geometry[nextGeometry++] = startVector.x;
+			geometry[nextGeometry++] = startVector.y;
+			geometry[nextGeometry++] = (float) screenX;
+			geometry[nextGeometry++] = (float) screenY;
+		}
+		return true;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		lightOrigin.set(screenX, screenY);
+		return true;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		if (amount > 0) {
+			lightRange *= 1.1;
+		} else {
+			lightRange /= 1.1;
+		}
+		return true;
 	}
 }
